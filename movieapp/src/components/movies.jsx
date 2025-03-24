b6d78abb993f67023af456317a63809f1a7c8937
lsh1752 <leesunghyuck0@gmail.com>
2025-03-24 21:44:30 +0900

    feat: 20250324 movie app component 분리

---
+++ movieapp/src/components/movies.jsx
@@ -0,0 +1,13 @@
+export default function Movie({key, coverImg, title, summary, genres}) {
+
+  return(
+    <div key={key}>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre) => (<li key={genre}>{genre}</li>))}
+      </ul>
+    </div>
+  );
+}
